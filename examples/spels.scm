e01399d135ec609233d6b2abd973730666c20999
Dane Lipscombe <danelipscombe@gmail.com>
2023-11-20 10:56:42 +1100

    Update

--- examples/spels.scm
+++ examples/spels.scm
@@ -14,11 +14,8 @@
 
 (define *location* 'living-room)
 
-(define (string-join l)
-  (apply to-string (map symbol->string l)))
-
 (define (describe-location location map)
-  (string-join (second (assoc location map))))
+  (second (assoc location map)))
 
 (define (describe-path path)
   `(there is a ,(second path) going ,(first path) from here.))
@@ -26,9 +23,9 @@
 ; (displayln (describe-location `living-room *map*))
 
 (define (describe-paths location m)
-  (apply to-string (map string-join (map describe-path (cddr (assoc location m))))))
+  (apply append (map describe-path (cddr (assoc location m)))))
 
-; (displayln (describe-paths 'living-room *map*))
+(displayln (describe-paths 'living-room *map*))
 
 (define (is-at obj loc obj-loc)
   (equal? (second (assoc obj obj-loc)) loc))
@@ -36,17 +33,15 @@
 ; (displayln (is-at 'whiskey-bottle 'living-room *object-locations*))
 
 (define (describe-floor loc objs obj-loc)
-  (apply to-string
-         (map string-join
-              (map (lambda (x) `(you see a ,x on the floor.))
-                   (filter (lambda (x) (is-at x loc obj-loc)) objs)))))
-
+  (apply append
+         (map (lambda (x) `(you see a ,x on the floor.))
+              (filter (lambda (x) (is-at x loc obj-loc)) objs))))
 ; (displayln (describe-floor 'living-room *objects* *object-locations*))
 
 (define (look)
-  (to-string (describe-location *location* *map*)
-             (describe-paths *location* *map*)
-             (describe-floor *location* *objects* *object-locations*)))
+  (describe-location *location* *map*)
+  (describe-paths *location* *map*)
+  (describe-floor *location* *objects* *object-locations*))
 
 (displayln (look))
 
@@ -56,7 +51,7 @@
       [next
        (set! *location* (third next))
        (look)]
-      [else "you cant go that way."])))
+      [else '(you cant go that way.)]))) ; TODO escape backqoutes
 
 ; (walk-direction 'west)
 ; (displayln (look))
@@ -65,6 +60,72 @@
   (syntax-rules ()
     [(walk direction) (walk-direction 'direction)]))
 
-(displayln (walk west))
-(displayln (walk west))
-; (displayln (look))
+(define (pickup-object object)
+  (cond
+    [(is-at object *location* *object-locations*)
+     (set! *object-locations* (cons (list object 'body) *object-locations*))
+     `(you are now carrying the ,object)]
+    [else '(you cannot get that.)]))
+
+(define-syntax pickup
+  (syntax-rules ()
+    [(pickup direction) (pickup-object 'direction)]))
+
+(define (inventory)
+  (filter (lambda (x) (is-at x 'body *object-locations*)) *objects*))
+
+(define (have object)
+  (member object (inventory)))
+
+(define *chain-welded* #f)
+
+; (define (weld subject object)
+;   (cond
+;     [(and (eq? *location* 'attic)
+;           (eq? subject 'chain)
+;           (eq? object 'bucket)
+;           (have 'chain)
+;           (have 'bucket)
+;           (not *chain-welded*))
+;      (set! *chain-welded* #t)
+;      '(the chain is now securely welded to the bucket.)]
+;     [else '(you cannot weld like that.)]))
+
+(define *bucket-filled* #f)
+
+; (define (dunk subject object)
+;   (cond
+;     [(and (eq? *location* 'garden)
+;           (eq? subject 'bucket)
+;           (eq? object 'well)
+;           (have 'bucket)
+;           *chain-welded*)
+;      (set! *bucket-filled* #t)
+;      '(the bucket is now full of water)]
+;     [else '(you cannot dunk like that.)]))
+
+; Error: Not yet implemented
+(define-syntax game-action
+  (syntax-rules ()
+    [(game-action command subj obj place rest...)
+     (define-syntax command
+       (syntax-rules ()
+         [(command subject object)
+          (cond
+            [(and (eq? *location* 'place) (eq? 'subject 'subj) (eq? 'object 'obj) (have 'subj)) rest]
+            [else '(i cant command like that.)])]))]))
+
+(game-action
+ weld
+ chain
+ bucket
+ attic
+ (cond
+   [(and (have 'bucket) (set! *chain-welded* #t)) '(the chain is now securely welded to the bucket.)]
+   [else '(you do not have a bucket.)]))
+
+(displayln (pickup whiskey-bottle))
+; (displayln (walk west))
+; (displayln (walk west))
+
+(displayln (weld 'chain 'bucket))
